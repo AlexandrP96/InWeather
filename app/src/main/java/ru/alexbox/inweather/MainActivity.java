8a7989dfbd5e767a03ab282823f0b4fe7d3f70e3
Alex Podgurski <alexbox726@gmail.com>
2020-12-06 22:41:27 +0200

    added stuff

--- app/src/main/java/ru/alexbox/inweather/MainActivity.java
+++ app/src/main/java/ru/alexbox/inweather/MainActivity.java
@@ -3,12 +3,30 @@ package ru.alexbox.inweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView city_text_view;
+
+    private Button button_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        onButtonAdd();
+    }
+
+    private void initView() {
+        city_text_view = findViewById(R.id.city_text_view);
+        button_add = findViewById(R.id.button_add);
+    }
+
+    private void onButtonAdd() {
+        button_add.setOnClickListener(v -> city_text_view.setText("Moscow"));
     }
 }
