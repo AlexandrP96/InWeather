9665fb60f907f3ba3b851f1d358af743586fa50f
Alex Podgurski <alexbox726@gmail.com>
2021-01-02 13:03:09 +0200

    final

--- app/src/main/java/ru/alexbox/inweather/view/MainActivity.java
+++ app/src/main/java/ru/alexbox/inweather/view/MainActivity.java
@@ -2,6 +2,7 @@ package ru.alexbox.inweather.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
